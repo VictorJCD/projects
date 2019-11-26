f1b1aedb401f12d43e085975d7e443c639c78301
VictorJCD <viccubdia@uma.es>
2019-11-26 22:56:46 +0100

    vid19, variables + use strict2

---
+++ holaMundoJS/js/variables.js
@@ -0,0 +1,13 @@
+// variables
+'use strict' // si quito el use stric5t, se podria declarar las variables sin declarar el tipo o sin el var*
+
+var pais = "España";
+var continente = "Europa";
+var antiguedad = 2019;
+var pais_y_continente = pais+' '+continente;
+
+pais = "Mexico";
+continentes = "America del sur";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
